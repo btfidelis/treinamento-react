0ce7c3e724772b25f7c2cb5cefe1ff6f2f1c8a9a
Bruno Fidelis <bruno.fidelis@kinghost.com.br>
2018-08-22 15:13:09 -0300

    step 1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
 import './App.css';
 
-const connectFn = connect(store => ({
-  
-}))
-
-
 class App extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      email: '',
-      pass: ''
-    }
-  }
-
-
-  handleInput(inputName) {
-    const fn = e => {
-      const val = e.target.value
-      const state = {}
-      state[inputName] = val
-
-      this.setState(() => state)
-    }
-
-    return fn.bind(this)
-  }
 
   render() {
     const style = {
@@ -40,26 +13,10 @@ class App extends Component {
 
     return (
       <div style={style}>
-        <div>
-          <label>Email</label>
-          <input 
-            type="text" 
-            onChange={this.handleInput('email')} 
-          />
-        </div>
-        <div>
-          <label>Senha</label>
-          <input 
-            type="password" 
-            onChange={this.handleInput('pass')} 
-          />
-        </div>
-        <div>
-          <button onClick={() => alert(this.state.email)}>aquii</button>
-        </div>
+
       </div>
     );
   }
 }
 
-export default connectFn(App);
+export default App;
